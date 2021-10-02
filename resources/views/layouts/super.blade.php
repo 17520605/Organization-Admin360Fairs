d02224a4611bbde2bef425d4d25e50fa1c48db78
Nguyễn Hữu Minh Khai <17520605@gm.uit.edu.vn>
2021-10-02 13:39:05 +0700

    Nguyễn Hữu Minh Khai Update

--- resources/views/layouts/super.blade.php
+++ resources/views/layouts/super.blade.php
@@ -13,5 +13,13 @@
         <a class="scroll-to-top rounded" href="#page-top">
             <i class="fas fa-angle-up"></i>
         </a>
+    <script>
+        function onActiveTourCard() {
+            let icon = document.querySelector('.icon_show_hide_card');
+            let card_body = document.querySelector('.card_body');
+            icon.classList.toggle('active');
+            card_body.classList.toggle('active');
+        }
+    </script>
     </body>
 </html>
