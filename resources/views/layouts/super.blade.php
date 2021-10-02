817431ebb705492c67d6c8edbcfdddf2e3a0afab
Nguyễn Hữu Minh Khai <17520605@gm.uit.edu.vn>
2021-10-02 16:23:38 +0700

    K

--- resources/views/layouts/super.blade.php
+++ resources/views/layouts/super.blade.php
@@ -13,6 +13,12 @@
         <a class="scroll-to-top rounded" href="#page-top">
             <i class="fas fa-angle-up"></i>
         </a>
+    <!-- Bootstrap core JavaScript-->
+    <script src="{{ asset('admin-master/asset/vendor/jquery/jquery.min.js')}}"></script>
+    <script src="{{ asset('admin-master/asset/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+    <!-- Core plugin JavaScript-->
+    <script src="{{ asset('admin-master/asset/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+
     <script>
         function onActiveTourCard() {
             let icon = document.querySelector('.icon_show_hide_card');
