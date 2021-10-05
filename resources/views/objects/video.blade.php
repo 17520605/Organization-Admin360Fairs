cd640160baac2636e7276377f705cf1d391ec784
Nguyễn Hữu Minh Khai <17520605@gm.uit.edu.vn>
2021-10-05 17:19:51 +0700

    Nguyễn Hữu Minh Khai Update

---
+++ resources/views/objects/video.blade.php
@@ -0,0 +1,170 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="container-fluid gallery">
+        <!-- Page Heading -->
+        <!-- DataTales Example -->
+        <div class="row clearfix">
+            <div class="col-lg-3 col-md-4 col-sm-12">
+                <div class="card">
+                    <div class="file">
+                        <a href="javascript:void(0);">
+                            <div class="hover">
+                                <button type="button" class="btn btn-icon btn-danger">
+                            <i class="fa fa-trash"></i>
+                        </button>
+                            </div>
+                            <div class="icon">
+                                <i class="fas fa-film"></i>
+                            </div>
+                            <div class="file-name">
+                                <p class="m-b-5 text-muted">hellonew.mkv</p>
+                                <small>Size: 720MB <span class="date text-muted">Dec 08, 2017</span></small>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-12">
+                <div class="card">
+                    <div class="file">
+                        <a href="javascript:void(0);">
+                            <div class="hover">
+                                <button type="button" class="btn btn-icon btn-danger">
+                            <i class="fa fa-trash"></i>
+                        </button>
+                            </div>
+                            <div class="icon">
+                                <i class="fab fa-youtube text-danger"></i>
+                            </div>
+                            <div class="file-name">
+                                <p class="m-b-5 text-muted">Jee Le Zara Song.mpg4</p>
+                                <small>Size: 32MB <span class="date text-muted">Oct 11, 2016</span></small>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-12">
+                <div class="card">
+                    <div class="file">
+                        <a href="javascript:void(0);">
+                            <div class="hover">
+                                <button type="button" class="btn btn-icon btn-danger">
+                                <i class="fa fa-trash"></i>
+                            </button>
+                            </div>
+                            <div class="icon">
+                                <i class="fas fa-film"></i>
+                            </div>
+                            <div class="file-name">
+                                <p class="m-b-5 text-muted">hellonew.mkv</p>
+                                <small>Size: 720MB <span class="date text-muted">Dec 08, 2017</span></small>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-12">
+                <div class="card">
+                    <div class="file">
+                        <a href="javascript:void(0);">
+                            <div class="hover">
+                                <button type="button" class="btn btn-icon btn-danger">
+                            <i class="fa fa-trash"></i>
+                        </button>
+                            </div>
+                            <div class="icon">
+                                <i class="fas fa-film"></i>
+                            </div>
+                            <div class="file-name">
+                                <p class="m-b-5 text-muted">hellonew.mkv</p>
+                                <small>Size: 720MB <span class="date text-muted">Dec 08, 2017</span></small>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-12">
+                <div class="card">
+                    <div class="file">
+                        <a href="javascript:void(0);">
+                            <div class="hover">
+                                <button type="button" class="btn btn-icon btn-danger">
+                            <i class="fa fa-trash"></i>
+                        </button>
+                            </div>
+                            <div class="icon">
+                                <i class="fab fa-youtube text-danger"></i>
+                            </div>
+                            <div class="file-name">
+                                <p class="m-b-5 text-muted">Jee Le Zara Song.mpg4</p>
+                                <small>Size: 32MB <span class="date text-muted">Oct 11, 2016</span></small>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-12">
+                <div class="card">
+                    <div class="file">
+                        <a href="javascript:void(0);">
+                            <div class="hover">
+                                <button type="button" class="btn btn-icon btn-danger">
+                            <i class="fa fa-trash"></i>
+                        </button>
+                            </div>
+                            <div class="icon">
+                                <i class="fab fa-youtube text-danger"></i>
+                            </div>
+                            <div class="file-name">
+                                <p class="m-b-5 text-muted">Jee Le Zara Song.mpg4</p>
+                                <small>Size: 32MB <span class="date text-muted">Oct 11, 2016</span></small>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-12">
+                <div class="card">
+                    <div class="file">
+                        <a href="javascript:void(0);">
+                            <div class="hover">
+                                <button type="button" class="btn btn-icon btn-danger">
+                            <i class="fa fa-trash"></i>
+                        </button>
+                            </div>
+                            <div class="icon">
+                                <i class="fas fa-film"></i>
+                            </div>
+                            <div class="file-name">
+                                <p class="m-b-5 text-muted">hellonew.mkv</p>
+                                <small>Size: 720MB <span class="date text-muted">Dec 08, 2017</span></small>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-12">
+                <div class="card">
+                    <div class="file">
+                        <a href="javascript:void(0);">
+                            <div class="hover">
+                                <button type="button" class="btn btn-icon btn-danger">
+                            <i class="fa fa-trash"></i>
+                        </button>
+                            </div>
+                            <div class="icon">
+                                <i class="fab fa-youtube text-danger"></i>
+                            </div>
+                            <div class="file-name">
+                                <p class="m-b-5 text-muted">Jee Le Zara Song.mpg4</p>
+                                <small>Size: 32MB <span class="date text-muted">Oct 11, 2016</span></small>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
