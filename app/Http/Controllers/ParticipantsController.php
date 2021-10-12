d828432adc8d5ec7892c24aba02de1ab40f6ac43
orics <nguyenngockhai25@gmail.com>
2021-10-13 00:36:22 +0700

    commit

--- app/Http/Controllers/ParticipantsController.php
+++ app/Http/Controllers/ParticipantsController.php
@@ -12,12 +12,64 @@ class ParticipantsController extends Controller
     public function index($id)
     {
         $tour = DB::table('tour')->find($id);
-        return view('participants.index', ['user' => Auth::user(), 'tour'=> $tour]);
+        $participants = DB::table('tour_participant')
+            ->join('profile', 'profile.id', '=', 'tour_participant.participantId')
+            ->where([
+                ['tour_participant.tourId', '=', $id],
+            ])
+            ->select('profile.*')
+            ->get();
+
+        return view('participants.index', ['user' => Auth::user(), 'tour'=> $tour, 'participants' => $participants]);
     }
     
-    public function saveCreate($id)
+    public function saveCreate($id, Request $request)
     {
         $tour = DB::table('tour')->find($id);
+
+        $name = $request->name;
+        $email = $request->email;
+        $contact = $request->contact;
         
+
+        $participantId = DB::table('tour_participant')
+            ->join('profile', 'profile.id', '=', 'tour_participant.participantId')
+            ->where([
+                ['tour_participant.tourId', '=', $id],
+                ['profile.email', '=', $email]
+            ])
+            ->select('profile.id')
+            ->first();
+        
+        if(!isset($participantId)){ // khong phai la doi tac
+            $profile = DB::table('profile')->where('email', $email)->first();
+            if(!isset($profile )){ // chua có tài khoản
+                // tao user
+                $user = new \App\Models\User();
+                $user->type = 'participant';
+                $user->email = $email;
+                $user->isRequiredChangePassword = true;
+                $user->save();
+                // tao profile
+                $profile = new \App\Models\Profile();
+                $profile->userId = $user->id;
+                $profile->name = $name;
+                $profile->email = $email;
+                $profile->contact = $contact;
+                $profile->save();
+            }
+
+            $participate = new \App\Models\Tour_Participant();
+            $participate->tourId = $id;
+            $participate->participantId = $profile->id;
+            $participate->save();
+
+            return true;
+        }
+        else{
+            return response("Da ton tai.");
+        }
+        
+        return false;
     }
 }
--- resources/views/participants/index.blade.php
+++ resources/views/participants/index.blade.php
@@ -5,34 +5,42 @@
         <div class="card-header">
             <h6 class="mb-0 font-weight-bold text-primary float-left">Participants</h6>
             <h6 class="mb-0 btn float-right" data-toggle="modal" data-target="#popup-create-participant"><i class="fas fa-plus"></i> Add </h6>
+            <h6 class="mb-0 btn float-right" data-toggle="modal" data-target="#popup-create-participant"><i class="fas fa-email"></i> Send Mail </h6>
+            <h6 class="mb-0 btn float-right" data-toggle="modal" data-target="#popup-create-participant"><i class="fas fa-email"></i> Import </h6>
         </div>
         <div class="card-body">
             <div class="table-responsive">
                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                     <thead>
                         <tr>
+                            <th style="width: 40px;">
+                                <input class="checkbox-all" type="checkbox" name="all">
+                            </th>
                             <th>#</th>
                             <th>Name</th>
                             <th>Email</th>
                             <th>Contact</th>
-                            <th style="width: 100px">Location</th>
                             <th style="width: 80px;">Status</th>
                             <th style="width: 80px;">Actions</th>
                         </tr>
                     </thead>
                     <tbody>
+                        @foreach ($participants as $participant)
                         <tr>
+                            <td>
+                                <input class="checkbox" type="checkbox" value="{{$participant->id}}" name="participantIds[]">
+                            </td>
                             <td>1</td>
-                            <td>Brielle Williamson</td>
-                            <td>nguyenhuuminhkhai@gmail.com</td>
-                            <td>0969888999</td>
-                            <td>Hà nội</td>
+                            <td>{{$participant->name}}</td>
+                            <td>{{$participant->email}}</td>
+                            <td>{{$participant->contact}}</td>
                             <td>Joinded</td>
                             <td>
                                 <a class="mr-2"><i class="fa fa-pen"></i></a>
                                 <a class="mr-0"><i class="fa fa-trash"></i></a>
                             </td>
                         </tr>
+                        @endforeach
                     </tbody>
                 </table>
             </div>
@@ -42,7 +50,7 @@
 <div class="modal fade" id="popup-create-participant" tabindex="-1" role="dialog" aria-hidden="true">
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
-            <form class="needs-validation" action="participants/save"  method="POST" novalidate>
+            <form id="popup-create-participant__form" class="needs-validation" novalidate>
                 <div class="modal-header">
                     <h5 class="fw-light">Create Participant </h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
@@ -68,13 +76,6 @@
                         <input type="tel" name="contact" class="form-control form-control-user" id="contact" placeholder="Enter Contact" aria-describedby="inputGroupPrepend" required>
                         <div class="invalid-feedback">
                             Please enter contact
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="name" class="form-label">Address</label>
-                        <input type="text" name="address" class="form-control form-control-user" id="address" placeholder="Enter address" aria-describedby="inputGroupPrepend" required>
-                        <div class="invalid-feedback">
-                            Please enter participant address
                         </div>
                     </div>
                 </div>
@@ -110,5 +111,29 @@
             false
         );
     })();
+</script>
+<script>
+    $(document).ready(function () {
+        $('#popup-create-participant__form').submit(function (e) { 
+            e.preventDefault();
+            let data = $(this).serialize();
+            $.ajax({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                },
+                type: "POST",
+                url: "/tours/{{$tour->id}}/participants/save-create",
+                data: data,
+                success: function (response) {
+                    if(response == true || response == "1"){
+                        location.reload();
+                    }
+                    else{
+                        alert(response);
+                    }
+                }
+            });
+        });
+    });
 </script>
 @endsection
