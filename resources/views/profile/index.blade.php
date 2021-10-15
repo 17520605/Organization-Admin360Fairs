1ee78c38c8dbf28c426cf025ec95d9c7eab56b21
Nguyễn Hữu Minh Khai <17520605@gm.uit.edu.vn>
2021-10-15 22:04:15 +0700

    Khai

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -38,7 +38,7 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="fw-light">Create New Profile Organization</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
               <div class="modal-body">
                     <div class="d-flex flex-column" style="flex-grow: 1;">
