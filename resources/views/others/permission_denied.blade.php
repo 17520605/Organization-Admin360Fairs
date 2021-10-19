3ad26fc38212aa76a9317b3167456389ce62d93b
Nguyễn Hữu Minh Khai <17520605@gm.uit.edu.vn>
2021-10-19 16:06:00 +0700

    Nguyễn Hữu Minh Khai Update

--- resources/views/others/permission_denied.blade.php
+++ resources/views/others/permission_denied.blade.php
@@ -4,9 +4,83 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@200;300;400;600;700;800;900&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+    <title>404 Page Not Found</title>
 </head>
+<style>
+/*======================
+    404 page
+=======================*/
+
+
+.page_404{ padding:40px 0; background:#fff; font-family: 'Nunito Sans', sans-serif;
+}
+
+.page_404  img{ width:100%;}
+
+.four_zero_four_bg{
+    background-image: url(https://cdn.dribbble.com/users/2182116/screenshots/13933572/media/cc32730b1eb3a657a48a6ceacefc72fb.gif);
+    height: 400px;
+    background-position: center;
+ }
+ 
+ 
+.four_zero_four_bg h1{
+    font-size:80px;
+}
+ 
+.four_zero_four_bg h3{
+	font-size:80px;
+}
+			 
+.link_404{			 
+	color: #fff!important;
+    background: #4e73df;
+    margin: 20px 0;
+    display: inline-block;
+}
+.contant_box_404{ 
+margin-top:-50px;
+}
+.link_404{
+    text-decoration: none;
+    padding: 10px 35px ;
+    border-radius: 5px;
+    cursor: pointer;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+}
+.link_404:hover{
+background: #224097;
+    text-decoration: none;
+}
+</style>
 <body>
-    <h1>Permission Denied !</h1>
+    <section class="page_404">
+	<div class="container">
+		<div class="row">	
+		<div class="col-sm-12 ">
+		<div class="col-sm-10 col-sm-offset-1  text-center">
+		<div class="four_zero_four_bg">
+			
+		
+		
+		</div>
+		<div class="contant_box_404">
+            <h3 class="h2">
+                Look like you're lost
+            </h3>
+            
+            <p>the page you are looking for not avaible!</p>
+            
+            <a href="http://admin.360fairs.com/" class="link_404">Go to Home</a>
+        </div>
+		</div>
+		</div>
+		</div>
+	</div>
+</section>
 </body>
 </html>
--- resources/views/speaker/tours/index.blade.php
+++ resources/views/speaker/tours/index.blade.php
@@ -5,30 +5,24 @@
             <div style="position: relative; margin-bottom: 3%;">
                 <h3>Hi, <span>{{$profile->name}}</span> 👋 </h3>
             </div>
-            <div class="border_card">
-                <div class="card_header">
-                    <span>Tours</span>
-                    <i onclick="onActiveTourCard()" class="icon_show_hide_card fas fa-chevron-up"></i>
-                </div>
-               <div class="row partner_card">
-                    <div class="col-lg-12">
-                        <div class="card card-margin">
-                            <div class="card-body pt-4">
-                                <div class="widget-49">
-                                    <div class="widget-49-title-wrapper" style="padding-bottom: 20px;">
-                                        <div class="widget-49-date-primary">
-                                            <span class="widget-49-date-day">09</span>
-                                            <span class="widget-49-date-month">apr</span>
-                                        </div>
-                                        <div class="widget-49-meeting-info">
-                                            <span style="font-weight: bold; font-size: 20px; color: #5462fc;cursor: pointer;">CTTNHH ABC</span>
-                                            <span class="widget-49-pro-title font-weight-bold">PRO-08235 DeskOpe. Website</span>
-                                        </div>
+            <div class="row partner_card">
+                <div class="col-lg-12">
+                    <div class="card card-margin">
+                        <div class="card-body pt-4">
+                            <div class="widget-49">
+                                <div class="widget-49-title-wrapper" style="padding-bottom: 20px;">
+                                    <div class="widget-49-date-primary">
+                                        <span class="widget-49-date-day">09</span>
+                                        <span class="widget-49-date-month">apr</span>
                                     </div>
-                                    <div class="widget-49-meeting-action px-4 py-3 border-top" style="position: relative;">
-                                        <li class="list-inline-item me-3" style="position: absolute;left: 0;"><i class="fas fa-calendar-check" style="color: #5462fc; margin-right: 5px; "></i> Date Mangager : 2019-10-15 - 14:00</li>
-                                        <a class="btn btn-df" style="position: absolute; right: 0; top: 0.5rem;border-radius: 3px;"> Manager</a>
+                                    <div class="widget-49-meeting-info">
+                                        <span style="font-weight: bold; font-size: 20px; color: #5462fc;cursor: pointer;">CTTNHH ABC</span>
+                                        <span class="widget-49-pro-title font-weight-bold">PRO-08235 DeskOpe. Website</span>
                                     </div>
+                                </div>
+                                <div class="widget-49-meeting-action px-4 py-3 border-top" style="position: relative;">
+                                    <li class="list-inline-item me-3" style="position: absolute;left: 0;"><i class="fas fa-calendar-check" style="color: #5462fc; margin-right: 5px; "></i> Date Mangager : 2019-10-15 - 14:00</li>
+                                    <a class="btn btn-df" style="position: absolute; right: 0; top: 2rem;border-radius: 3px;"> Manager</a>
                                 </div>
                             </div>
                         </div>
