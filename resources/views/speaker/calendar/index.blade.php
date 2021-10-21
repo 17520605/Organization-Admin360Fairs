fe46350fdf1b70940412d4562d384fda6d63d990
Nguyễn Hữu Minh Khai <17520605@gm.uit.edu.vn>
2021-10-21 17:28:38 +0700

    Khai up

--- resources/views/speaker/calendar/index.blade.php
+++ resources/views/speaker/calendar/index.blade.php
@@ -8,7 +8,37 @@
                 <div class="card">
                     <div class="card-body">
                         <div class="row">
-                            <div class="col-lg-12">
+                            <div class="col-lg-3">
+                                <div class="d-grid">
+                                    <button class="btn btn-lg font-16 btn-danger" id="btn-new-event"><i class="mdi mdi-plus-circle-outline"></i> Create New Event</button>
+                                </div>
+                                <div id="external-events" class="m-t-20">
+                                    <br>
+                                    <p class="text-muted">Drag and drop your event or click in the calendar
+                                    </p>
+                                    <div class="external-event bg-success-lighten text-success" data-class="bg-success">
+                                        <i class="mdi mdi-checkbox-blank-circle me-2 vertical-middle"></i>New Theme Release
+                                    </div>
+                                    <div class="external-event bg-info-lighten text-info" data-class="bg-info">
+                                        <i class="mdi mdi-checkbox-blank-circle me-2 vertical-middle"></i>My Event
+                                    </div>
+                                    <div class="external-event bg-warning-lighten text-warning" data-class="bg-warning">
+                                        <i class="mdi mdi-checkbox-blank-circle me-2 vertical-middle"></i>Meet manager
+                                    </div>
+                                    <div class="external-event bg-danger-lighten text-danger" data-class="bg-danger">
+                                        <i class="mdi mdi-checkbox-blank-circle me-2 vertical-middle"></i>Create New theme
+                                    </div>
+                                </div>
+                                <div class="mt-5 d-none d-xl-block">
+                                    <h5 class="text-center">How It Works ?</h5>
+                                    <ul class="ps-3">
+                                        <li class="text-muted mb-3">
+                                            It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col-lg-9">
                                 <div class="mt-4 mt-lg-0">
                                     <div id="calendar" class="fc fc-media-screen fc-direction-ltr fc-theme-bootstrap" style="height: 531px;">
                                     </div>
