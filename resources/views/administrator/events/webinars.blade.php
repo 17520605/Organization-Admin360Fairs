853dc4b3dda7dfb6dcdba43516a2f1f6bda979fd
Nguyễn Hữu Minh Khai <17520605@gm.uit.edu.vn>
2021-10-28 20:42:09 +0700

    Nguyễn Hữu Minh Khai Update

--- resources/views/administrator/events/webinars.blade.php
+++ resources/views/administrator/events/webinars.blade.php
@@ -4,10 +4,76 @@
     <div class="container-fluid ">
         <h1 class="h3 text-gray-800">Events <button class="btn btn-df" style="position: absolute; right: 1.5rem;" data-toggle="modal" data-target="#popup-create-webinar"><i class="fas fa-upload" style="margin-right: 8px;"></i> Add new event</button></h1>
         <div class="tab-header mb-3" style="width: 100%; height: 40px; mb-2 ">
-            <span id="tab-event-btn-line" class="btn btn-primary float-left active" data-tab="table"><i class="fas fa-stream"></i></span>
-            <span id="tab-event-btn-card" class="btn btn-primary float-left " data-tab="card"><i class="fas fa-clone"></i></span>
+            <span id="tab-event-btn-line1" class="btn btn-primary float-left"><i class="fas fa-stream"></i></span>
+            <span id="tab-event-btn-line" class="btn btn-primary float-left active"><i class="fas fa-stream"></i></span>
+            <span id="tab-event-btn-card" class="btn btn-primary float-left"><i class="fas fa-clone"></i></span>
         </div>
         <div id="event-show-line" class="row line-time" style="display: block;">
+            <div class="col-lg-12">
+                    <div class="card-body">
+                        <div class="row justify-content-center">
+                            <div class="col-xl-10">
+                                <div class="timeline">
+                                    <div class="timeline-container">
+                                        <div class="timeline-end">
+                                            <p>Start</p>
+                                        </div>
+                                        <div class="timeline-continue">
+                                            
+                                            @foreach ($webinars as $webinar)
+                                            <div class="row timeline-box-card">
+                                                @php
+                                                    $time = Carbon\Carbon::parse($webinar->startAt);
+                                                @endphp
+                                                <div class="col-md-6">
+                                                    <div class="timeline-icon">
+                                                        {{-- <i class="bx bx-briefcase-alt-2 text-primary h2 mb-0"></i> --}}
+                                                    </div>
+                                                    <div class="timeline-box">
+                                                        <div class="timeline-date bg-primary text-center rounded">
+                                                            <h4 class="text-white mb-0">{{$time->format('d')}}</h4>
+                                                            <h5 class="text-white mb-0" style="text-transform: uppercase ;font-size: 16px">{{$time->format('M')}}</h5>
+                                                        </div>
+                                                        <div class="event-content">
+                                                            <div class="timeline-text">
+
+                                                                <a href="#" class="hover-a-webinar popov" 
+                                                                    data-toggle="popover" 
+                                                                    title="{{$webinar->topic}}" 
+                                                                    data-content=" 
+                                                                        @foreach ($webinar->details as $detail) 
+                                                                            <a class='text-detail-webinar'><i class='fas fa-check'></i>  {{$detail->title}} </a><br>
+                                                                        @endforeach
+                                                                    " 
+                                                                    data-html="true"><i class="fas fa-angle-right"></i> {{$webinar->topic}}
+                                                                </a>
+
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            @endforeach
+                                        </div>
+                                        <div class="timeline-start">
+                                            <p>End</p>
+                                        </div>
+                                        <div class="timeline-launch">
+                                            <div class="timeline-box">
+                                                <div class="timeline-text">
+                                                    <h3 style="font-size: 17px" class="text-primary font-weight-bold">Launched our company on 21 June 2021</h3>
+                                                    <p class="text-muted mb-0">Copyright © by 360Fairs</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+        </div>
+        <div id="event-show-line1" class="row line-time" style="display: block;">
             <div class="col-lg-12">
                     <div class="card-body">
                         <div class="row justify-content-center">
@@ -164,13 +230,25 @@
         $('#tab-event-btn-card').click(function(){
             $('#event-show-card').show();
             $('#event-show-line').hide();
+            $('#event-show-line1').hide();
             $('#tab-event-btn-card').addClass("active");
             $('#tab-event-btn-line').removeClass("active");
+            $('#tab-event-btn-line1').removeClass("active");
         });
         $('#tab-event-btn-line').click(function(){
             $('#event-show-card').hide();
             $('#event-show-line').show();
+            $('#event-show-line1').hide();
             $('#tab-event-btn-line').addClass("active");
+            $('#tab-event-btn-card').removeClass("active");
+            $('#tab-event-btn-line1').removeClass("active");
+        });
+        $('#tab-event-btn-line1').click(function(){
+            $('#event-show-card').hide();
+            $('#event-show-line').hide();
+            $('#event-show-line1').show();
+            $('#tab-event-btn-line1').addClass("active");
+            $('#tab-event-btn-line').removeClass("active");
             $('#tab-event-btn-card').removeClass("active");
         });
     </script>
